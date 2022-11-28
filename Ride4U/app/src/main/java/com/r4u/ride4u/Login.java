cf0cafe0b1927a058c1b8ca04329f1f98c653edc
MoriyaBarel <16moriya16@gmail.com>
2022-11-28 16:43:36 +0200

    reg button

--- Ride4U/app/src/main/java/com/r4u/ride4u/Login.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/Login.java
@@ -1,7 +1,10 @@
 package com.r4u.ride4u;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        configureNextButton();
     }
+
+
+    private void configureNextButton() {
+        Button reg_button = (Button)findViewById(R.id.Go_to_register);
+        reg_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+    }
+
+
 }
