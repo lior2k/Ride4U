701fea62623317971b8a0d64369110504837cd6d
MoriyaBarel <16moriya16@gmail.com>
2022-12-06 22:15:48 +0200

    Password reset via email implemented
    login, register and forgot impl with auth

--- Ride4U/app/src/main/java/com/r4u/ride4u/Login.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/Login.java
@@ -7,7 +7,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,8 +20,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
-
-    DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://ride4u-3a773-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
+    FirebaseAuth autoProfile;
+//    DatabaseReference databaseReference = FirebaseDatabase.getInstance("https://ride4u-3a773-default-rtdb.europe-west1.firebasedatabase.app/").getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,46 +34,35 @@ public class Login extends AppCompatActivity {
     }
 
     private void setupLoginButton() {
-        final TextInputLayout id = findViewById(R.id.id);
+        final TextInputLayout email = findViewById(R.id.email);
         final TextInputLayout password = findViewById(R.id.password);
         final Button loginBtn = findViewById(R.id.loginBtn);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final EditText idETxt = id.getEditText();
+                final EditText emailETxt = email.getEditText();
                 final EditText passwordETxt = password.getEditText();
 
                 // convert fields to string variables and check that they're not empty
-                if (idETxt != null && passwordETxt != null) {
-                    if (idETxt.getText() != null && passwordETxt.getText() != null) {
-                        final String idTxt = idETxt.getText().toString();
+                if (emailETxt != null && passwordETxt != null) {
+                    if (emailETxt.getText() != null && passwordETxt.getText() != null) {
+                        final String emailTxt = emailETxt.getText().toString();
                         final String passwordTxt = passwordETxt.getText().toString();
 
-                        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        //login with authentication
+                        autoProfile = FirebaseAuth.getInstance();
+                        autoProfile.signInWithEmailAndPassword(emailTxt, passwordTxt).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
-                            public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                                // check if id exists in the system - we are using id as unique identifier for each user.
-                                if (snapshot.hasChild(idTxt)) {
-                                    // check if password entered matches with password in our database
-                                    final String getPassword = snapshot.child(idTxt).child("password").getValue(String.class);
-                                    if (passwordTxt.equals(getPassword)) {
-                                        Toast.makeText(Login.this, "Successful log in", Toast.LENGTH_SHORT).show();
-                                        // go to main screen
-                                        startActivity(new Intent(Login.this, MainActivity.class));
-                                        finish();
-                                    } else {
-                                        Toast.makeText(Login.this, "Wrong password", Toast.LENGTH_SHORT).show();
-                                    }
-                                } else {
-                                    Toast.makeText(Login.this, "Wrong ID", Toast.LENGTH_SHORT).show();
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getApplicationContext(),"Login successful!", Toast.LENGTH_LONG).show();
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
                                 }
-
-                            }
-
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError error) {
-
+                                else {
+                                    // Login failed
+                                    Toast.makeText(getApplicationContext(),"Login failed!! Please try again later", Toast.LENGTH_LONG).show();
+                                }
                             }
                         });
                     }
