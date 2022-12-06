8530dedf158dc5d04851ffc550c59458b6b63783
Lior <Lior2kx@gmail.com>
2022-12-06 16:11:37 +0200

    Navigation Bar
    Added new Navigation bar to MainActivity, Nav bar colors not changing. \nAdded simple Post class. \nAdded empty fragments.

--- Ride4U/app/src/main/java/com/r4u/ride4u/MainActivity.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/MainActivity.java
@@ -1,14 +1,69 @@
 package com.r4u.ride4u;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import androidx.fragment.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.r4u.ride4u.Fragment.AddFragment;
+import com.r4u.ride4u.Fragment.HomeFragment;
+import com.r4u.ride4u.Fragment.ProfileFragment;
+import com.r4u.ride4u.Fragment.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(navigationItemSelectedListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
     }
+
+    private BottomNavigationView.OnItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnItemSelectedListener() {
+                @SuppressLint("NonConstantResourceId")
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+
+                        case R.id.nav_add:
+                            selectedFragment = null;
+//                            startActivity(new Intent(MainActivity.this, PostActivity.class)); // start the add post activity
+                            break;
+
+                        case R.id.nav_profile:
+//                            SharedPreferences.Editor editor = getSharedPreferences("PREFS", MODE_PRIVATE).edit();
+//                            editor.putString("profileId", FirebaseAuth.getInstance().getCurrentUser().getUid()); dependant on moriya adding firebase auth
+//                            editor.apply();
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                    }
+
+                    return false;
+                }
+
+            };
+
 }
