9963f6c0ba6284356140268e592a4cc23dcdd23f
chai <chai9l@gmail.com>
2022-12-12 20:03:39 +0200

    profile-undone

--- Ride4U/app/src/main/java/com/r4u/ride4u/Fragment/ProfileFragment.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/Fragment/ProfileFragment.java
@@ -8,9 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
 import com.r4u.ride4u.R;
-
+// https://www.youtube.com/watch?v=O17fvn7kztg&ab_channel=UiLoverAndroid - profile tutorial
 public class ProfileFragment extends Fragment {
+
+    private String email;
+    private String name;
+    private String last_name;
+    private int id;
+    DatabaseReference databaseReference;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
