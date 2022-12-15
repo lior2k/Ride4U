bede6306a881b3ac36b9bd743d7fa3decbfe5ac4
MoriyaBarel <16moriya16@gmail.com>
2022-12-15 20:22:35 +0200

    unfinished admin operations

--- Ride4U/app/src/main/java/com/r4u/ride4u/Fragment/ProfileFragment.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/Fragment/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.r4u.ride4u.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,8 +17,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.r4u.ride4u.AdminOperations;
 import com.r4u.ride4u.Login;
+import com.r4u.ride4u.PostActivity;
 import com.r4u.ride4u.R;
+import com.r4u.ride4u.Register;
 
 public class ProfileFragment extends Fragment {
 
@@ -41,6 +45,13 @@ public class ProfileFragment extends Fragment {
         adminBtn = view.findViewById(R.id.admin_options);
         if (!Login.user.getIsAdmin()) {
             adminBtn.setVisibility(View.INVISIBLE);
+        } else {
+            adminBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startActivity(new Intent(getContext(), AdminOperations.class));
+                }
+            });
         }
     }
 
