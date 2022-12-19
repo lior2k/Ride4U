3a5502642553b82944b17ebdadca57b26d83881b
MoriyaBarel <16moriya16@gmail.com>
2022-12-19 14:44:13 +0200

    comments

--- Ride4U/app/src/main/java/com/r4u/ride4u/UserActivities/ForgotPassword.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/UserActivities/ForgotPassword.java
@@ -17,6 +17,8 @@ import com.r4u.ride4u.R;
 public class ForgotPassword extends AppCompatActivity {
 
     DatabaseReference databaseReference = FirebaseDatabase.getInstance(Login.firebase_url).getReference();
+    String emailTxt;
+    String idTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,11 @@ public class ForgotPassword extends AppCompatActivity {
         setupBackBtn();
     }
 
+    /**
+     Set up the button for sending a new password.
+     Set an OnClickListener for the sendNewPWBtn Button when clicked, set the emailTxt
+     and idTxt variables to the text in the email and id.
+     */
     private void setupSendNewPasswordButton() {
         final TextInputLayout email = findViewById(R.id.email);
         final TextInputLayout id = findViewById(R.id.history_);
@@ -37,32 +44,11 @@ public class ForgotPassword extends AppCompatActivity {
 
             if (emailETxt != null && idETxt != null) {
                 if (emailETxt.getText().length() > 0 && idETxt.getText().length() > 0) {
-                    final String emailTxt = emailETxt.getText().toString();
-                    final String idTxt = idETxt.getText().toString();
+                    emailTxt = emailETxt.getText().toString();
+                    idTxt = idETxt.getText().toString();
 
-                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
-                            // check that id exists in the system
-                            if (snapshot.hasChild(idTxt)) {
-                                // check that email matches with existing email in database
-                                String dbEmail = snapshot.child(idTxt).child("email").getValue(String.class);
-                                if (emailTxt.equals(dbEmail)) {
-                                    resetPassword(emailTxt);
-                                } else {
-                                    Toast.makeText(ForgotPassword.this, "Email doesn't match", Toast.LENGTH_SHORT).show();
-                                }
-                            } else {
-                                Toast.makeText(ForgotPassword.this, "ID not found", Toast.LENGTH_SHORT).show();
-                            }
-                        }
-
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError error) {
-                            System.out.println(error.getCode());
-                        }
-
-                    });
+                    // Check the email and id on the database.
+                    checkIdAndEmailOnDatabase();
 
                 } else {
                     Toast.makeText(ForgotPassword.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
@@ -71,14 +57,47 @@ public class ForgotPassword extends AppCompatActivity {
         });
     }
 
+    /**
+     Check the email and ID on the database to see if they match an existing user.
+     If a match is found, reset the user's password.
+     */
+    private void checkIdAndEmailOnDatabase() {
+        databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // check that id exists in the system
+                if (snapshot.hasChild(idTxt)) {
+                    // check that email matches with existing email in database
+                    String dbEmail = snapshot.child(idTxt).child("email").getValue(String.class);
+                    if (emailTxt.equals(dbEmail)) {
+                        resetPassword();
+                    } else {
+                        Toast.makeText(ForgotPassword.this, "Email doesn't match", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(ForgotPassword.this, "ID not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println(error.getCode());
+            }
+
+        });
+    }
+
     private void setupBackBtn() {
         final Button backBtn = findViewById(R.id.backBtn);
         backBtn.setOnClickListener(view -> finish());
     }
 
-    private void resetPassword(String email) {
+    /**
+     Reset the user's password by sending a password reset email to the user's email address.
+     */
+    private void resetPassword() {
         FirebaseAuth authProfile = FirebaseAuth.getInstance();
-        authProfile.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+        authProfile.sendPasswordResetEmail(emailTxt).addOnCompleteListener(task -> {
             if(task.isSuccessful()) {
                 Toast.makeText(ForgotPassword.this, "please check your inbox for password reset link", Toast.LENGTH_SHORT).show();
                 finish();
