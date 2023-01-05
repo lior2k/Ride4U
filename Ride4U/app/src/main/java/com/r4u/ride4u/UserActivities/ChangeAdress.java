90edc688c5196de64a6e95fbfee9db00648108d4
chai9l <chai9l@gmail.com>
2023-01-05 16:34:38 +0200

    starting profile functionality

---
+++ Ride4U/app/src/main/java/com/r4u/ride4u/UserActivities/ChangeAdress.java
@@ -0,0 +1,16 @@
+package com.r4u.ride4u.UserActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.r4u.ride4u.R;
+
+public class ChangeEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_email2);
+    }
+}
---
+++ Ride4U/app/src/main/java/com/r4u/ride4u/UserActivities/ChangePassword.java
@@ -0,0 +1,14 @@
+package com.r4u.ride4u.UserActivities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangePassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password2);
+    }
+}
