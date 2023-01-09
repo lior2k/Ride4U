714913b382eec8672733aa7ea9e971a25a25d2ed
chai9l <chai9l@gmail.com>
2023-01-09 14:54:13 +0200

    Change Password DONE

--- Ride4U/app/src/main/java/com/r4u/ride4u/Fragment/ProfileFragment.java
+++ Ride4U/app/src/main/java/com/r4u/ride4u/Fragment/ProfileFragment.java
@@ -1,29 +1,41 @@
 package com.r4u.ride4u.Fragment;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.r4u.ride4u.AdminActivities.Operations;
 //import com.r4u.ride4u.UserActivities.ChangeAdress;
-import com.r4u.ride4u.UserActivities.EditProfile;
 import com.r4u.ride4u.UserActivities.Login;
 import com.r4u.ride4u.R;
 
 public class ProfileFragment extends Fragment {
 
+    private final FirebaseAuth authProfile = FirebaseAuth.getInstance();
     //    private String user_password;
     TextView profileName;
     TextView profileEmail;
+    TextView profileAddress;
     Button adminBtn;
-    Button editProfile;
 
     // This function creates the screen using the data pulled from the functions below.
     @Override
@@ -32,8 +44,8 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         initProfile(view);
-        setEditProfileBtn(view);
         setAdminBtn(view);
+        setupChangePassBtn(view);
         return view;
     }
 
@@ -53,15 +65,81 @@ public class ProfileFragment extends Fragment {
         }
     }
 
-    private void setEditProfileBtn(View view) {
-        editProfile = view.findViewById(R.id.edit_profile);
-        editProfile.setOnClickListener(new View.OnClickListener() {
+
+    ProgressDialog loadingBar;
+    private void showChangePasswordDialog() {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setTitle("Change Password");
+        LinearLayout linearLayout=new LinearLayout(this.getContext());
+        final EditText emailet= new EditText(this.getContext());
+
+        // write the email using which you registered
+        emailet.setHint("Email");
+        emailet.setMinEms(16);
+        emailet.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        linearLayout.addView(emailet);
+        linearLayout.setPadding(10,10,10,10);
+        builder.setView(linearLayout);
+
+        // Click on Recover and a email will be sent to your registered email id
+        builder.setPositiveButton("Change", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getContext(), EditProfile.class));
+            public void onClick(DialogInterface dialog, int which) {
+                String email=emailet.getText().toString().trim();
+                beginChange(email);
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
+    private void beginChange(String email) {
+        loadingBar=new ProgressDialog(this.getContext());
+        loadingBar.setMessage("Sending Email....");
+        loadingBar.setCanceledOnTouchOutside(false);
+        loadingBar.show();
+
+        // calling sendPasswordResetEmail
+        // open your email and write the new
+        // password and then you can login
+        authProfile.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                loadingBar.dismiss();
+                if(task.isSuccessful())
+                {
+                    // if isSuccessful then done message will be shown
+                    // and you can change the password
+                    Toast.makeText(ProfileFragment.this.getContext(),"Password change link has been sent",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(ProfileFragment.this.getContext(),"Error Occurred",Toast.LENGTH_LONG).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                loadingBar.dismiss();
+                Toast.makeText(ProfileFragment.this.getContext(),"Error Failed",Toast.LENGTH_LONG).show();
             }
         });
     }
+
+    private void setupChangePassBtn(View view) {
+        final TextView changePass = view.findViewById(R.id.change_pass_text);
+        changePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showChangePasswordDialog();
+            }
+        });
+    }
+
     // Initialize the profile's name and email acoording to the set database.
 
     private void initProfile(View view) {
